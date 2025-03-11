82286a722e0266fcca54521adab0045beadf9640
capmahin <mahinulhuque1996@gmail.com>
2025-03-12 04:02:56 +0600

    cancle the substrcition

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -5,5 +5,23 @@ const subscriptionRouter = Router();
 subscriptionRouter.get("/", (req, res) =>
   res.send({ title: "GET all subscriptions" })
 );
+subscriptionRouter.get("/:id", (req, res) =>
+  res.send({ title: "GET subscription details " })
+);
+subscriptionRouter.post("/", (req, res) =>
+  res.send({ title: "CREATE subscriptions" })
+);
+subscriptionRouter.put("/:id", (req, res) =>
+  res.send({ title: "UPDATE subscriptions" })
+);
+subscriptionRouter.delete("/:id", (req, res) =>
+  res.send({ title: "DELETE subscriptions" })
+);
+subscriptionRouter.get("/user/:id", (req, res) =>
+  res.send({ title: "GET all user subscriptions" })
+);
+subscriptionRouter.put("/:id/cancel", (req, res) =>
+  res.send({ title: "CANCEL subscriptions" })
+);
 
 export default subscriptionRouter;
