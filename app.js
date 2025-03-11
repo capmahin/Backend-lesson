607b49b0a996bc6d683cc8040c8665949ee54623
capmahin <mahinulhuque1996@gmail.com>
2025-03-12 03:52:01 +0600

    add routes

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ import express from "express";
 
 import { PORT } from "./config/env.js";
 
-const app = express();
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
+const app = express();
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
