00c358819619af1905eb034a2195ce1f3a284ba0
capmahin <mahinulhuque1996@gmail.com>
2025-03-13 00:50:45 +0600

    add timestand true

--- models/user.model.js
+++ models/user.model.js
@@ -1,24 +1,27 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "User Name is required"],
-    trim: true,
-    minLength: 2,
-    maxLength: 50
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "User Name is required"],
+      trim: true,
+      minLength: 2,
+      maxLength: 50
+    },
+    email: {
+      type: String,
+      required: [true, "User Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: [/\S+@\S+\.\S+/, "Please fill a valid email address"]
+    },
+    password: {
+      type: String,
+      required: [true, "User Password is required"],
+      minLength: 6
+    }
   },
-  email: {
-    type: String,
-    required: [true, "User Email is required"],
-    unique: true,
-    trim: true,
-    lowercase: true,
-    match: [/\S+@\S+\.\S+/, "Please fill a valid email address"]
-  },
-  password: {
-    type: String,
-    required: [true, "User Password is required"],
-    minLength: 6
-  }
-});
+  { timestamps: true }
+);
