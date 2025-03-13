25f99046803c479bae5ee70382cb4b2ba28dcff3
capmahin <mahinulhuque1996@gmail.com>
2025-03-14 01:16:42 +0600

    add another massage

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -4,6 +4,11 @@ const errorMiddleware = (err, req, res, next) => {
 
     error.message = err.message;
     console.error(err);
+
+    //   Mongoose bad ObjectId
+    if (err.name === "CastError") {
+      const message = "Resource not found";
+    }
   } catch (error) {
     next(error);
   }
