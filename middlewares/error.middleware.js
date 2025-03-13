132c9cb64462ee12ed3c56960f414afa3cb7a079
capmahin <mahinulhuque1996@gmail.com>
2025-03-14 02:11:08 +0600

    add another error

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -10,6 +10,14 @@ const errorMiddleware = (err, req, res, next) => {
       const message = "Resource not found";
 
       error = new Error(message);
+      error.statusCode = 404;
+    }
+
+    //   Mongoose duplicate key
+    if (err.code === 1100) {
+      const message = "Duplicate field value entered";
+      error = new Error(message);
+      error.statusCode = 400;
     }
   } catch (error) {
     next(error);
