c5652f0787b42fec22aaea6391db9b4d2ed75a61
capmahin <mahinulhuque1996@gmail.com>
2025-03-15 00:54:36 +0600

    add a signup

---
+++ controllers/auth.controller.js
@@ -0,0 +1,7 @@
+export const signUp = async (req, resizeBy, next) => {
+  // Implement sign up logic here
+};
+
+export const signIn = async (req, resizeBy, next) => {};
+
+export const signOut = async (req, res, next) => {};
