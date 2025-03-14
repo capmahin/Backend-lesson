f9552ee41385fc160e44919b7bb72c081dfdacea
capmahin <mahinulhuque1996@gmail.com>
2025-03-15 02:55:07 +0600

    add anodeer

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,9 @@
+import mongoose from "mongoose";
+
 export const signUp = async (req, resizeBy, next) => {
   // Implement sign up logic here
+  const session = await mongoose.startSession();
+  session.startTransaction();
 };
 
 export const signIn = async (req, resizeBy, next) => {};
