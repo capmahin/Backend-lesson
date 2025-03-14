e739ea39e4af799d7e0f5c58377e223b942c2768
capmahin <mahinulhuque1996@gmail.com>
2025-03-15 04:47:36 +0600

    threw a error

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,24 @@
 import mongoose from "mongoose";
+import User from "./../models/user.model.js";
 
 export const signUp = async (req, resizeBy, next) => {
   // Implement sign up logic here
   const session = await mongoose.startSession();
   session.startTransaction();
   try {
+    // Logic to create a new user
+    await session.commitTransaction();
+
+    const { name, email, password } = req.body;
+
+    //Check if a user already exists
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      const error = new Error("User already exists");
+      error.statusCode = 409;
+      throw error;
+    }
   } catch (error) {
     await session.abortTransaction();
     session.endSession();
