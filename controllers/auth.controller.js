9a87e37b2b8eafc79a49e5850d03a26cafb02d8d
capmahin <mahinulhuque1996@gmail.com>
2025-03-15 04:25:02 +0600

    add try catch for session

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,12 @@ export const signUp = async (req, resizeBy, next) => {
   // Implement sign up logic here
   const session = await mongoose.startSession();
   session.startTransaction();
+  try {
+  } catch (error) {
+    await session.abortTransaction();
+    session.endSession();
+    next(error);
+  }
 };
 
 export const signIn = async (req, resizeBy, next) => {};
